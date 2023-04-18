da396735d48f23fcee85b03755c42eaad42f181d
alan <azc@live.unc.edu>
2023-04-17 20:05:33 -0400

    formatted the course catalog although still havent figured out how to do the department page

---
+++ src/components/DataArrays.js
@@ -0,0 +1,53 @@
+export const aArray = [
+  "AAAD",
+  "AERO",
+  "AMST",
+  "ANTH",
+  "APPL",
+  "ARAB",
+  "ARMY",
+  "ARTH",
+  "ARTS",
+  "ASIA",
+  "ASTR",
+];
+
+export const bArray = ["BIOC", "BIOL", "BIOS", "BMME", "BUSI"];
+
+export const cArray = [
+  "CBIO",
+  "CHEM",
+  "CHER",
+  "CHIN",
+  "CLAR",
+  "CLAS",
+  "CLSC",
+  "CMPL",
+  "COMM",
+  "COMP",
+  "CZCH",
+];
+
+export const courseArrays = {
+  AAADCourses: [
+    "101",
+    "130",
+    "159",
+    "200",
+    "201",
+    "212",
+    "214",
+    "231",
+    "232",
+    "237",
+    "240",
+    "250",
+    "252",
+    "254",
+    "257",
+  ],
+};
+
+export default function DataArrays() {
+  return null;
+}
