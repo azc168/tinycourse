f4b767bdf15d70db624dfce1735539609eac70bc
alan <azc@live.unc.edu>
2023-04-20 12:53:06 -0400

    added json file and displayed couses on the department page

--- src/components/DepartmentPage.js
+++ src/components/DepartmentPage.js
@@ -1,23 +1,65 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { courseArrays } from "./DataArrays";
+import classData from "./classData.json";
+import "./departmentPage.css";
 
 export default function DepartmentPage() {
   const { department } = useParams();
-  const departmentCourses = courseArrays[department + "Courses"];
+  let departmentCourses = [];
+  switch (department) {
+    case "COMP":
+      departmentCourses = classData.Comp;
+      break;
+    case "BIOL":
+      departmentCourses = classData.Biol;
+      break;
+    case "ECON":
+      departmentCourses = classData.Econ;
+      break;
+    default:
+      break;
+  }
 
   return (
     <div>
-      <h1>{department}</h1>
-
-      {/* Check if departmentCourses is defined before mapping */}
-      {departmentCourses && departmentCourses.map((departmentCourse, index) => (
-        <Link key={index} to={`/department/${department}/${departmentCourse}`}>
-          {departmentCourse}
-        </Link>
-      ))}
-
-      {/* Add any additional content or components here */}
+      <div className="class-card-container">
+        {departmentCourses.length > 0 ? (
+          departmentCourses.map((course, index) => (
+            <div key={index} className="class-card">
+              <div className="class-header">
+                <div className="class-department">
+                  {department} {course.Number}
+                </div>
+              </div>
+              <div className="class-name">{course.Name}</div>
+              <div className="class-rating"> Rating:</div>
+              <div className="class-workload"> Workload:</div>
+              <div className="class-difficulty"> Difficulty:</div>
+              <div>
+                {Array.isArray(course.Tags) && course.Tags.length > 0 && (
+                  <div>
+                    {course.Tags.map((tag) => (
+                      <div key={tag} className="class-gen-ed">
+                        {tag}
+                      </div>
+                    ))}
+                  </div>
+                )}
+                {typeof course.Tags === "string" && course.Tags !== "N/A" && (
+                  <div className="class-gen-ed">{course.Tags}</div>
+                )}
+              </div>
+              <div className="class-review-link">
+                <Link to={`/department/${department}/${course.Number}/reviews`}>
+                  <u>Read more reviews</u>
+                </Link>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No classes uploaded yet. Please check back at a later time.</div>
+        )}
+      </div>
     </div>
   );
 }
