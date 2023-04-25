dfe2bf795608553b0123f8b8d50c382e9128fc87
Hannah Chisholm <hschisho@live.unc.edu>
2023-04-25 09:31:36 -0400

    sorting start

--- src/components/DepartmentPage.js
+++ src/components/DepartmentPage.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../firebase-config";
 import { Link, useParams } from "react-router-dom";
 import classData from "./classData.json";
 import "./departmentPage.css";
 // import { getDocs, collection, query, where } from "firebase/firestore";
 
 export default function DepartmentPage() {
-  const { department } = useParams();
+  const [sortValue, setSortValue] = useState("");
+  const [genValue, setGenValue] = useState("Any");
+  const params = useParams();
+  const [, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  /*const { courseDep, courseNum, rating, workload } = {
+    courseDep: params.department.toUpperCase(),
+    courseNum: params.courseNum,
+    rating: params.rating,
+    workload: params.workload,
+  };*/
+
+  
   let departmentCourses = [];
-  switch (department) {
+  switch (params.department) {
     case "COMP":
       departmentCourses = classData.Comp;
       break;
@@ -19,10 +33,30 @@ export default function DepartmentPage() {
       break;
     default:
       break;
-  }
+  };
 
-  const filterSearch = () => {
-  }
+  
+  useEffect(() => {
+
+    console.log(sortValue);
+    if (sortValue === "high-low-course-num") {
+      departmentCourses.sort(function(a, b){return b.Number - a.Number});
+    } else if (sortValue === "low-high-course-num") {
+      departmentCourses.sort(function(a, b){return a.Number - b.Number});
+    }
+
+    forceUpdate();
+
+    console.log(genValue);
+    //console.log(departmentCourses.filter((course) => {return course.Tags === 'PX';}));
+    //departmentCourses.filter((course) => course.Tags === 'PX');
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortValue, genValue]);
+  
+
+  // if i filter the department courses, then when i map them they will already be in order
+
 
   return (
     <div className="page-body">
@@ -31,7 +65,7 @@ export default function DepartmentPage() {
             {" "}
             Sort: {" "}
         </label>
-        <select name="sortOptions" id="high-low">
+        <select name="sortOptions" onChange={(event) => {setSortValue(event.target.value)}} value={sortValue} id="high-low">
           <option value="low-high-course-num">Low-high course number</option>
           <option value="high-low-course-num">High-low course number</option>
           <option value="high-low-rating">High-low rating</option>
@@ -43,14 +77,15 @@ export default function DepartmentPage() {
             {" "}
             Filter by Gen Ed: {" "}
         </label>
-        <select name="genedOptions" id="gen-ed">
+        <select name="genedOptions" onChange={(event) => {setGenValue(event.target.value)}} value={genValue} id="gen-ed">
           <option value="Any">Any</option>
-          <option value="PH">PH</option>
-          <option value="QI">QI</option>
+          <option value="CI">CI</option>
+          <option value="EE">EE</option>
+          <option value="PL">PL</option>
+          <option value="PX">PX</option>
           <option value="QR">QR</option>
-          <option value="WB">WB</option>
         </select>
-        <button className="filter-search" onClick={filterSearch}>Find me classes!</button>
+        <button className="filter-search">Find me classes!</button>
       </div>
       <div className="class-card-container">
         {departmentCourses.length > 0 ? (
@@ -58,7 +93,7 @@ export default function DepartmentPage() {
             <div key={index} className="class-card">
               <div className="class-header">
                 <div className="class-department">
-                  {department} {course.Number}
+                  {params.department} {course.Number}
                 </div>
               </div>
               <div className="class-name">{course.Name}</div>
@@ -80,7 +115,7 @@ export default function DepartmentPage() {
                 )}
               </div>
               <div className="class-review-link">
-                <Link to={`/department/${department}/${course.Number}/reviews`} state={{courseDep:department,courseNum:course.Number}}>
+                <Link to={`/department/${params.department}/${course.Number}/reviews`} state={{courseDep:params.department,courseNum:course.Number}}>
                   <u>Read more reviews</u>
                 </Link>
               </div>
