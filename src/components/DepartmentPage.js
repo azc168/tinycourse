85d281e7563e4a38b8a909e772bbb23d4ebb425d
Hannah Chisholm <hschisho@live.unc.edu>
2023-04-25 09:45:51 -0400

    fix merge conflicts

--- src/components/DepartmentPage.js
+++ src/components/DepartmentPage.js
@@ -20,6 +20,7 @@ export default function DepartmentPage() {
   };*/
 
   
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   let departmentCourses = [];
   switch (department) {
     case "COMP":
