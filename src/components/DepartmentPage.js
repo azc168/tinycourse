b0166a37ff82d3e97cb810a88289317fd40da1b9
alan <azc@live.unc.edu>
2023-04-26 14:39:41 -0400

    formatted department page

--- src/components/DepartmentPage.js
+++ src/components/DepartmentPage.js
@@ -4,6 +4,13 @@ import { db } from "../firebase-config";
 import { Link, useParams } from "react-router-dom";
 import classData from "./classData.json";
 import "./departmentPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faStarHalfAlt,
+  faBook,
+  faSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function DepartmentPage() {
   const [sortValue, setSortValue] = useState("low-high-course-num");
@@ -12,7 +19,6 @@ export default function DepartmentPage() {
   const [, updateState] = React.useState();
   const forceUpdate = React.useCallback(() => updateState({}), []);
 
-  
   // eslint-disable-next-line react-hooks/exhaustive-deps
   let departmentCourses = [];
   switch (department) {
@@ -27,26 +33,41 @@ export default function DepartmentPage() {
       break;
     default:
       break;
-  };
-  
-  useEffect(() => {
+  }
 
+  useEffect(() => {
     if (sortValue === "high-low-course-num") {
-      departmentCourses.sort(function(a, b){return b.Number - a.Number});
+      departmentCourses.sort(function (a, b) {
+        return b.Number - a.Number;
+      });
     } else if (sortValue === "low-high-course-num") {
-      departmentCourses.sort(function(a, b){return a.Number - b.Number});
+      departmentCourses.sort(function (a, b) {
+        return a.Number - b.Number;
+      });
     } else if (sortValue === "high-low-rating") {
-      departmentCourses.sort(function(a, b){return averageRatings[b.Number] - averageRatings[a.Number]});
+      departmentCourses.sort(function (a, b) {
+        return averageRatings[b.Number] - averageRatings[a.Number];
+      });
     } else if (sortValue === "low-high-rating") {
-      departmentCourses.sort(function(a, b){return averageRatings[a.Number] - averageRatings[b.Number]});
+      departmentCourses.sort(function (a, b) {
+        return averageRatings[a.Number] - averageRatings[b.Number];
+      });
     } else if (sortValue === "high-low-workload") {
-      departmentCourses.sort(function(a, b){return averageWorkload[b.Number] - averageWorkload[a.Number]});
+      departmentCourses.sort(function (a, b) {
+        return averageWorkload[b.Number] - averageWorkload[a.Number];
+      });
     } else if (sortValue === "low-high-workload") {
-      departmentCourses.sort(function(a, b){return averageWorkload[a.Number] - averageWorkload[b.Number]});
+      departmentCourses.sort(function (a, b) {
+        return averageWorkload[a.Number] - averageWorkload[b.Number];
+      });
     } else if (sortValue === "high-low-difficulty") {
-      departmentCourses.sort(function(a, b){return averageDiff[b.Number] - averageDiff[a.Number]});
+      departmentCourses.sort(function (a, b) {
+        return averageDiff[b.Number] - averageDiff[a.Number];
+      });
     } else if (sortValue === "low-high-difficulty") {
-      departmentCourses.sort(function(a, b){return averageDiff[a.Number] - averageDiff[b.Number]});
+      departmentCourses.sort(function (a, b) {
+        return averageDiff[a.Number] - averageDiff[b.Number];
+      });
     }
     forceUpdate();
 
@@ -54,7 +75,7 @@ export default function DepartmentPage() {
     //console.log(departmentCourses.filter((course) => {return course.Tags === 'PX';}));
     //departmentCourses.filter((course) => course.Tags === 'PX');
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sortValue, genValue]);
 
   const [avgRating, setAvgRating] = useState([]);
@@ -64,7 +85,6 @@ export default function DepartmentPage() {
   const [averageRatings, setAverageRatings] = useState({});
   const [averageWorkload, setAverageWorkload] = useState({});
   const [averageDiff, setAverageDiff] = useState({});
-
 
   const getAverages = async (department, courseNum) => {
     const courseRef = collection(db, "reviews");
@@ -93,7 +113,9 @@ export default function DepartmentPage() {
   useEffect(() => {
     if (departmentCourses.length > 0) {
       Promise.all(
-        departmentCourses.map((course) => getAverages(department, course.Number))
+        departmentCourses.map((course) =>
+          getAverages(department, course.Number)
+        )
       ).then((averages) => {
         setAvgRating(
           averages.map((avg) => {
@@ -118,11 +140,10 @@ export default function DepartmentPage() {
       });
     }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [department, departmentCourses]);
 
-
-  useEffect(() => { 
+  useEffect(() => {
     const updatedAverageRatings = {};
     for (var i = 0; i < courseNum.length; i++) {
       updatedAverageRatings[courseNum[i]] = avgRating[i];
@@ -130,7 +151,7 @@ export default function DepartmentPage() {
     setAverageRatings(updatedAverageRatings);
   }, [avgRating, courseNum, department, departmentCourses]);
 
-  useEffect(() => { 
+  useEffect(() => {
     const updatedAverageWorkload = {};
     for (var i = 0; i < courseNum.length; i++) {
       updatedAverageWorkload[courseNum[i]] = avgWorkload[i];
@@ -138,7 +159,7 @@ export default function DepartmentPage() {
     setAverageWorkload(updatedAverageWorkload);
   }, [avgWorkload, courseNum, department, departmentCourses]);
 
-  useEffect(() => { 
+  useEffect(() => {
     const updatedAverageDiff = {};
     for (var i = 0; i < courseNum.length; i++) {
       updatedAverageDiff[courseNum[i]] = avgDifficulty[i];
@@ -146,47 +167,159 @@ export default function DepartmentPage() {
     setAverageDiff(updatedAverageDiff);
   }, [avgDifficulty, courseNum, department, departmentCourses]);
 
+  function renderWorkloadRating(workload) {
+    const fullStars = Math.floor(workload);
+
+    return (
+      <div className="workloadRating">
+        {[...Array(5)].map((_, index) => {
+          if (index < fullStars) {
+            // Render a shaded star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faBook}
+                className="checkedBook"
+              />
+            );
+          } else {
+            // Render an empty star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faBook}
+                className="uncheckedBook"
+              />
+            );
+          }
+        })}
+      </div>
+    );
+  }
+
+  function renderStarRating(rate) {
+    const fullStars = Math.floor(rate);
+    const hasHalfStar = rate - fullStars >= 0.5;
+
+    return (
+      <div className="starRating">
+        {[...Array(5)].map((_, index) => {
+          if (index < fullStars) {
+            // Render a shaded star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faStar}
+                className="checkedStar"
+              />
+            );
+          } else if (index === fullStars && hasHalfStar) {
+            // Render a half shaded star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faStarHalfAlt}
+                className="checkedStar"
+              />
+            );
+          } else {
+            // Render an empty star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faStar}
+                className="uncheckedStar"
+              />
+            );
+          }
+        })}
+      </div>
+    );
+  }
+
+  function renderDifficultyRating(difficulty) {
+    const fullStars = Math.floor(difficulty);
+
+    return (
+      <div className="difficultyRating">
+        {[...Array(5)].map((_, index) => {
+          if (index < fullStars) {
+            // Render a shaded star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faSquare}
+                className="checkedSquare"
+              />
+            );
+          } else {
+            // Render an empty star
+            return (
+              <FontAwesomeIcon
+                key={index}
+                icon={faSquare}
+                className="uncheckedSquare"
+              />
+            );
+          }
+        })}
+      </div>
+    );
+  }
 
   return (
-    <div className="page-body">  
+    <div className="page-body">
       <div className="backBtnContainer">
         <div className="backButton">
           <Link to={`/`}>
-              <u id="backToCourses">Back to Course Catalog</u>
+            <u id="backToCourses">Back to Course Catalog</u>
           </Link>
         </div>
-      </div> 
-        {departmentCourses.length > 0 ? (
-          <div className="sort-bar">
-            <label>
-              {" "}
-              Sort: {" "}
-            </label><select className="sorting" name="sortOptions" onChange={(event) => { setSortValue(event.target.value); } } value={sortValue} id="high-low">
-                <option value="low-high-course-num">Low-high course number</option>
-                <option value="high-low-course-num">High-low course number</option>
-                <option value="high-low-rating">High-low rating</option>
-                <option value="low-high-rating">Low-high rating</option>
-                <option value="high-low-workload">High-low workload</option>
-                <option value="low-high-workload">Low-high workload</option>
-                <option value="high-low-difficulty">High-low difficulty</option>
-                <option value="low-high-difficulty">Low-high difficulty</option>
-              </select><label id="genId">
-                {" "}
-                Filter by Gen Ed: {" "}
-              </label><select className="gened" name="genedOptions" onChange={(event) => { setGenValue(event.target.value); } } value={genValue} id="gen-ed">
-                <option value="Any">Any</option>
-                <option value="CI">CI</option>
-                <option value="EE">EE</option>
-                <option value="PL">PL</option>
-                <option value="PX">PX</option>
-                <option value="QR">QR</option>
-              </select><button className="filter-search">
-                Apply Settings
-              </button>
-          </div>
+      </div>
+      {departmentCourses.length > 0 ? (
+        <div className="sort-bar">
+          <label> Sort: </label>
+          <select
+            className="sorting"
+            name="sortOptions"
+            onChange={(event) => {
+              setSortValue(event.target.value);
+            }}
+            value={sortValue}
+            id="high-low"
+          >
+            <option value="low-high-course-num">Low-high course number</option>
+            <option value="high-low-course-num">High-low course number</option>
+            <option value="high-low-rating">High-low rating</option>
+            <option value="low-high-rating">Low-high rating</option>
+            <option value="high-low-workload">High-low workload</option>
+            <option value="low-high-workload">Low-high workload</option>
+            <option value="high-low-difficulty">High-low difficulty</option>
+            <option value="low-high-difficulty">Low-high difficulty</option>
+          </select>
+          <label id="genId"> Filter by Gen Ed: </label>
+          <select
+            className="gened"
+            name="genedOptions"
+            onChange={(event) => {
+              setGenValue(event.target.value);
+            }}
+            value={genValue}
+            id="gen-ed"
+          >
+            <option value="Any">Any</option>
+            <option value="CI">CI</option>
+            <option value="EE">EE</option>
+            <option value="PL">PL</option>
+            <option value="PX">PX</option>
+            <option value="QR">QR</option>
+          </select>
+          <button className="filter-search">Apply Settings</button>
+        </div>
+      ) : (
         // eslint-disable-next-line jsx-a11y/heading-has-content
-        ) : ( <h1></h1>
-        )}
+        <h1></h1>
+      )}
       <div className="class-card-container">
         {departmentCourses.length > 0 ? (
           departmentCourses.map((course, index) => (
@@ -197,9 +330,28 @@ export default function DepartmentPage() {
                 </div>
               </div>
               <div className="class-name">{course.Name}</div>
-              <div className="class-rating"> Rating: {Math.round(averageRatings[course.Number])} / 5</div>
-              <div className="class-workload"> Workload: {Math.round(averageWorkload[course.Number])} / 5</div>
-              <div className="class-difficulty"> Difficulty: {Math.round(averageDiff[course.Number])} / 5</div>
+              <div className="class-rating">
+                <p className="rating">
+                  Rating:&nbsp;&nbsp;
+                  {renderStarRating(Math.round(averageRatings[course.Number]))}
+                </p>
+              </div>
+              <div className="class-workload">
+                <p className="rating">
+                  Workload:&nbsp;&nbsp;
+                  {renderWorkloadRating(
+                    Math.round(averageWorkload[course.Number])
+                  )}
+                </p>
+              </div>
+              <div className="class-difficulty">
+                <p className="rating">
+                  Difficulty:&nbsp;&nbsp;
+                  {renderDifficultyRating(
+                    Math.round(averageDiff[course.Number])
+                  )}
+                </p>
+              </div>
               <div>
                 {Array.isArray(course.Tags) && course.Tags.length > 0 && (
                   <div>
@@ -212,7 +364,8 @@ export default function DepartmentPage() {
                 )}
                 {typeof course.Tags === "string" && course.Tags !== "N/A" && (
                   <div className="class-gen-ed">
-                  <p>{course.Tags}</p></div>
+                    <p>{course.Tags}</p>
+                  </div>
                 )}
               </div>
               <div className="class-review-link">
