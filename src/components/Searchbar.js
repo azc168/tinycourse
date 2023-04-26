2a21adc8b962b67ad1c9685963e9edf7e478f574
alan <azc@live.unc.edu>
2023-04-26 18:22:02 -0400

    added support for classes with multiple gen eds in search bar.

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -8,36 +8,53 @@ export default function Searchbar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
 
-
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
     const newFilter = data.filter((value) => {
-      return value.Department.toUpperCase().includes(searchWord.toUpperCase())
-      || value.Number.includes(searchWord)
-      || value.Tags === searchWord.toUpperCase()
-      || value.Department.toUpperCase() + " " + value.Number === (searchWord.toUpperCase())
-      || value.Department.toUpperCase()+value.Number === (searchWord.toUpperCase())
+      return (
+        value.Department.toUpperCase().includes(searchWord.toUpperCase()) ||
+        value.Number.includes(searchWord) ||
+        (Array.isArray(value.Tags) && value.Tags.includes(searchWord.toUpperCase())) ||
+        (typeof value.Tags === 'string' && value.Tags.toUpperCase().includes(searchWord.toUpperCase())) ||
+        (Array.isArray(value.Tags) && value.Tags.some(tag => tag.toUpperCase().includes(searchWord.toUpperCase()))) ||
+        value.Department.toUpperCase() + " " + value.Number === searchWord.toUpperCase() ||
+        value.Department.toUpperCase() + value.Number === searchWord.toUpperCase()
+      );
     });
+    
 
-    if(searchWord === "") {
+    if (searchWord === "") {
       setFilteredData([]);
     } else {
       setFilteredData(newFilter);
     }
-  }
+  };
 
   const clearInput = () => {
     setFilteredData([]);
     setWordEntered("");
-  }
+  };
 
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} value={wordEntered} onChange={handleFilter}></input>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        ></input>
         <div className="searchIcon">
-        {filteredData.length === 0 ? <FontAwesomeIcon icon={faSearch} /> : <FontAwesomeIcon icon={faClose} id="clearBtn" onClick={clearInput}/>}
+          {filteredData.length === 0 ? (
+            <FontAwesomeIcon icon={faSearch} />
+          ) : (
+            <FontAwesomeIcon
+              icon={faClose}
+              id="clearBtn"
+              onClick={clearInput}
+            />
+          )}
         </div>
       </div>
       {filteredData.length !== 0 && (
@@ -49,7 +66,9 @@ export default function Searchbar({ placeholder, data }) {
                   className="dataItem"
                   to={`/department/${value.Department}/${value.Number}/reviews`}
                 >
-                  <p>{value["Department"].toUpperCase() + " " + value["Number"]}</p>
+                  <p>
+                    {value["Department"].toUpperCase() + " " + value["Number"]}
+                  </p>
                 </Link>
               </div>
             );
